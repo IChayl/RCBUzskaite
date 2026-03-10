1eb8c9349d9aff20db3b7c77d5027f3fa6f89e77
IChayl <Mareks.Rumjancevs@rezeknestehnikums.lv>
2026-03-10 11:09:10 +0000

    dsadsad

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -166,7 +166,7 @@
     <a href="/kustibas_veidi">Kustības veidi</a>
     <a href="/inventars">Inventārs</a>
     <a href="/telpa">Telpas</a>
-    <a href="/lietotajs">Lietotāji</a>
+    <a href="/lietotajs">Lietotājiiiiiiiii</a>
   </nav>
 
   <div class="auth-links">
