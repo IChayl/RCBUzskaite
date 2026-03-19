36eb9f858e0c801c42afb9d73ffb2a1bd6585e61
IChayl <Mareks.Rumjancevs@rezeknestehnikums.lv>
2026-03-19 07:02:15 +0000

    gfdgcvb

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -159,10 +159,10 @@
        @if(Auth::check())
       <a href="/inventars">Inventārs</a>
       <a href="/inventara_kustiba">Kustības</a>
+      <a href="/norakstishana">Norakstīšanas</a>
       <a href="/kustibas_veidi">Kustības veidi</a>
       <a href="/kategorija">Kategorijas tabula</a>
       <a href="/telpa">Telpas</a>
-      <a href="/norakstishana">Norakstīšanas</a>
       <a href="/lietotajs">Lietotāji</a>
       @else
          @endif
